05febdb21ea2545083ac8682823b932dc33c907a
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-28 22:23:28 +0300

    register models in admin

--- Backend/Counsellingapp/admin.py
+++ Backend/Counsellingapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Counselor, Client, SupportGroup, Session, Medication, MedicationDosage, update_session
 # Register your models here.
+
+
+admin.site.register(Counselor)
+admin.site.register(Client)
+admin.site.register(SupportGroup)
+admin.site.register(Session)
+admin.site.register(Medication)
+admin.site.register(MedicationDosage)
+admin.site.register(update_session)
+
