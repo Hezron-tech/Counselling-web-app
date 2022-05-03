906b8fde12cd4be5f52f5e6d999304bfb30ac93b
Joseph-Wairimu <josephkamore084@gmail.com>
2022-05-03 10:10:15 +0300

    update model

--- Backend/Counsellingapp/urls.py
+++ Backend/Counsellingapp/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(
 urlpatterns = [
 
     # swagger ui
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+    path('', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
