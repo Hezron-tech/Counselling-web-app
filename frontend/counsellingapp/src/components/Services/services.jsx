6999215e3ed203f13781f537bf9a4f17fadc1ae1
Joseph-Wairimu <josephkamore084@gmail.com>
2022-05-04 13:31:53 +0300

    services

---
+++ frontend/counsellingapp/src/components/Services/services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import servicesStyles from './services.css'
+
+export default function services() {
+  return (
+    <div >
+        
+        <p className='head'>ffghjk</p>
+        
+        
+        </div>
+  )
+}
