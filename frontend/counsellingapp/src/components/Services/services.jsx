ed77bfc0b07aeb9b6e89f14b49714eef8be34425
Joseph-Wairimu <josephkamore084@gmail.com>
2022-05-04 17:32:52 +0300

    service body

--- frontend/counsellingapp/src/components/Services/services.jsx
+++ frontend/counsellingapp/src/components/Services/services.jsx
@@ -5,11 +5,97 @@ import servicesStyles from './services.css'
 
 export default function services() {
   return (
-    <div >
+    <div className='body' >
     
         <h2 className='head'>Our <br/> Services</h2>
-        
-        
+        <p className='service'>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum a qui <br/> 
+perferendis quos hic molestias, corrupti numquam dolor ratione mollitia, maiores <br/>
+ dolore iusto tempora, iste officiis. Perferendis excepturi aut libero.</p>
+        <div className='serve'>
+           <div>
+                <div>
+                     
+                </div>
+                
+                <div>
+                    <h3>Psychology Therapy</h3>
+                    <p> Lorem ipsum dolor sit amet <br/> consectetur, adipisicing elit. <br/> Voluptatum a qui perferendis  <br/> quos hic molestias,</p>
+                    
+                </div>
+
+
+           </div>
+
+
+           <div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+                    <h3>Families</h3>
+                    <p> Lorem ipsum dolor sit amet <br/> consectetur, adipisicing elit. <br/> Voluptatum a qui </p>
+                    
+                    </div>
+               
+           </div>
+
         </div>
+        <div className='serve'>
+                <div>
+                        <div>
+                            
+                        </div>
+                        
+                        <div>
+                            <h3>Individuals</h3>
+                            <p> Lorem ipsum dolor sit amet <br/> consectetur, adipisicing elit. <br/> Voluptatum a qui perferendis  <br/> quos hic molestias,</p>
+                            
+                        </div>
+
+
+                </div>
+
+
+                <div>
+
+                            <div>
+
+                            </div>
+
+                            <div>
+                            <h3>Couples</h3>
+                            <p> Lorem ipsum dolor sit amet <br/> consectetur, adipisicing elit. <br/> Voluptatum a qui </p>
+                            
+                            </div>
+                    
+                </div>
+
+        </div>
+        <div className='approach'>
+
+                 <div>
+                
+                 </div>
+                 <div>
+                    <h3>A Holistic Approach</h3>
+                    <h4>The path to mental wellbeing is different 
+                         for everyone</h4>
+                         <p>We take a personal and considerate  approach to <br/> treating  each patient’s  whole being. Just as great<br/> athletes build up their  physical fitness, we help <br/> those we serve build up mental health<br/>
+                           by considering a wide range of factors when<br/> developing a personalised mental healthcare plan.</p>
+                        <h4 className='decorate'>Learn More #</h4>
+                 </div>
+
+
+
+
+
+
+
+
+        </div>
+  </div>
   )
 }
