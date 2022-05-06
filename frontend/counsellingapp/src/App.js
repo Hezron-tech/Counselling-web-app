ac2e115a5ad283640024f5d0d390894963d0e5a0
Joseph-Wairimu <josephkamore084@gmail.com>
2022-05-06 11:09:28 +0300

    complete routing

--- frontend/counsellingapp/src/App.js
+++ frontend/counsellingapp/src/App.js
@@ -1,10 +1,10 @@
 
 import React from "react";
-import {  Router, Link,  Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router ,Routes, Route } from 'react-router-dom';
   
 import Navbar from "./components/Navbar/Navbar";
 import About from './components/About/About';
-
+import Login from './components/login';
 import Register from "./components/register";
 import Services from './components/Services/services';
 import Landing  from "./components/Landing/landing";
@@ -14,20 +14,24 @@ import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
+        <>
+        <Router>
+          
+             <Navbar/>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="about" element={<About />} />
+              <Route path="services" element={<Services />} />
+              <Route path="register" element={<Register />} />
+              <Route path="login" element={<Login />} />
+              <Route path="*" element={<ErrorPage/>} />
+            </Routes>
 
-      //  <Navbar/>
-   
-      //  <Landing/>
-    <Router>
-      <Register/>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="register" element={<Register />} />
-        <Route path="*" element={<ErrorPage/>} />
-      </Routes>
-    </Router>
+        </Router>
+        
+    
+        </>
+  
   );
 
 }
---
+++ frontend/counsellingapp/src/components/login.js
@@ -0,0 +1,51 @@
+import React, { useState,useEffect } from 'react'
+// import { useNavigate } from 'react-router-dom';
+import Loginn from  './login'
+
+function Login(){
+
+
+    
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    // const history = useNavigate();
+    // useEffect(() =>{
+    //     if (localStorage.getItem('user-info')){
+    //         history.push("/home")
+    //     }
+    // },
+    // [])
+
+    async function Login(){
+        console.warn(email,password);
+        let item= {email,password};
+        let result=await fetch('http://127.0.0.1:8000/rest-auth/login/', {
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify(item)
+        });
+        result = await result.json();
+        // localStorage.setItem( "user-info",JSON.stringify(result))
+        // history.push("/home")
+    }
+    return(
+        <div>
+            <Loginn/>
+        <h1>Login Here</h1>
+        <div className='col-sm-6 offset-sm-3'>
+            <input type="text" placeholder='email' onChange={(e)=>setEmail(e.target.value)} className='form-control'/> <br/>
+            <input type="password" placeholder='password' onChange={(e)=>setPassword(e.target.value)} className='form-control'/> <br/>
+
+            <button onClick={Login} className='btn btn-primary'>Login </button>
+
+        </div>
+
+        </div>
+        
+
+    )
+}
+export default Login;
